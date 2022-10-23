fac001d72e213594f727d9bece41375d48dd2ed1
David Grayson <daves.grays@gmail.com>
2022-10-23 16:22:48 -0700

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,11 @@ env_name=goku
 python_version=3.10
 
 conda create -n ${env_name} python=${python_version}
-source activate ${env_name}
+CONDA_BASE=$(conda info --base)
+source $CONDA_BASE/etc/profile.d/conda.sh
+conda activate $env_name
+
 pip install -r requirements.txt
 pip install -e .
 python -m ipykernel install --name ${env_name}
-source deactivate
+conda deactivate
