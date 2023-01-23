cfdc1c6832ba12c477db1105014983d347079c54
David Grayson <daves.grays@gmail.com>
2023-01-23 00:09:00 -0800

    shrink network

--- diffusion_from_scratch/unet.py
+++ diffusion_from_scratch/unet.py
@@ -27,7 +27,7 @@ class DoubleConv(nn.Module):
 
 
 class Down(nn.Module):
-    def __init__(self, in_channels, out_channels, emb_dim=256):
+    def __init__(self, in_channels, out_channels, emb_dim=128):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
             nn.MaxPool2d(2),
@@ -47,7 +47,7 @@ class Down(nn.Module):
 
 
 class Up(nn.Module):
-    def __init__(self, in_channels, out_channels, emb_dim=256):
+    def __init__(self, in_channels, out_channels, emb_dim=128):
         super().__init__()
 
         self.up = nn.Upsample(scale_factor=2, mode="bilinear", align_corners=True)
@@ -95,24 +95,26 @@ class SelfAttention(nn.Module):
 
 
 class UncondUNet(nn.Module):
-    def __init__(self, c_in=3, c_out=3, time_dim=256):
+    def __init__(self, c_in=3, c_out=3, time_dim=128):
         super().__init__()
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.time_dim = time_dim
         self.inc = DoubleConv(c_in, 64)
         self.down1 = Down(64, 128)
-        self.sa1 = SelfAttention(128, 32)
+        # self.sa1 = SelfAttention(128, 32)
         self.down2 = Down(128, 256)
-        self.sa2 = SelfAttention(256, 16)
-        self.down3 = Down(256, 256)
-        self.sa3 = SelfAttention(256, 8)
+        # self.sa2 = SelfAttention(256, 16)
+        self.down3 = Down(256, 512)
+        # self.sa3 = SelfAttention(256, 8)
 
-        self.up1 = Up(512, 128)
-        self.sa4 = SelfAttention(128, 16)
-        self.up2 = Up(256, 64)
-        self.sa5 = SelfAttention(64, 32)
+        self.bot = DoubleConv(512, 512)
+
+        self.up1 = Up(512, 256)
+        # self.sa4 = SelfAttention(128, 16)
+        self.up2 = Up(256, 128)
+        # self.sa5 = SelfAttention(64, 32)
         self.up3 = Up(128, 64)
-        self.sa6 = SelfAttention(64, 64)
+        # self.sa6 = SelfAttention(64, 64)
         self.outc = nn.Conv2d(64, c_out, kernel_size=1)
 
     def pos_encoding(self, t, channels):
@@ -131,16 +133,17 @@ class UncondUNet(nn.Module):
 
         x1 = self.inc(x)
         x2 = self.down1(x1, t)
-        x2 = self.sa1(x2)
+        # x2 = self.sa1(x2)
         x3 = self.down2(x2, t)
-        x3 = self.sa2(x3)
+        # x3 = self.sa2(x3)
         x4 = self.down3(x3, t)
-        x4 = self.sa3(x4)
+        # x4 = self.sa3(x4)
+        x4 = self.bot(x4)
         x = self.up1(x4, x3, t)
-        x = self.sa4(x)
+        # x = self.sa4(x)
         x = self.up2(x, x2, t)
-        x = self.sa5(x)
+        # x = self.sa5(x)
         x = self.up3(x, x1, t)
-        x = self.sa6(x)
+        # x = self.sa6(x)
         output = self.outc(x)
         return output
