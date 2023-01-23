4ab8fb0a44abe00eef1d398724425aaf4da04ab7
David Grayson <daves.grays@gmail.com>
2023-01-22 18:25:03 -0800

    pre-commit hooks

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="mypackage",
     version="0.1",
-    description='GANs for image2image translation',
-    author='David Grayson',
+    description="DMs for image2image translation",
+    author="David Grayson",
     packages=find_packages(),
 )
