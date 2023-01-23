1e3a8a97e060fb82f10c5470428877396d27b67b
David Grayson <daves.grays@gmail.com>
2023-01-22 20:44:07 -0800

    cuda and sampler code

--- diffusion_from_scratch/train.py
+++ diffusion_from_scratch/train.py
@@ -5,22 +5,23 @@ from torch.utils.data import DataLoader
 from unet import UncondUNet
 
 
-# TODO: put everything on cuda
 class DiffusionModel:
     def __init__(self, t_steps=1000, epochs=10):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         # from original ddpm paper
         # betas and alphas deal with noise schedule
         scale = 1000 / t_steps
         self.beta_start = scale * 0.0001
         self.beta_end = scale * 0.02
         self.t_steps = t_steps
-        self.betas = torch.linspace(self.beta_start, self.beta_end, self.t_steps)
+        self.betas = torch.linspace(self.beta_start, self.beta_end, self.t_steps).to(
+            self.device
+        )
         self.alpha_hats = torch.cumprod(1 - self.betas, dim=0)
         # model
         self.epochs = epochs
         self.batch_size = 16
         self.model = None
-        self.device = "cuda" if torch.cuda.is_available() else "cpu"
         # data
         self.dataset_path = "/Users/davidg/Projects/goku/data/miyazaki"
         self.image_size = 64
@@ -29,25 +30,30 @@ class DiffusionModel:
         return torch.randint(self.t_steps, size=1)
 
     def add_noise_one_step(self, x, t):
-        # from original ddpm paper
+        # from original ddpm paper (Algorithm 1)
         rescaled_x = x * torch.sqrt(self.alpha_hat[t])
-        noise = torch.randn_like(x)
+        noise = torch.randn_like(x).to(self.device)
         scaled_noise = noise * torch.sqrt(1 - self.alpha_hat[t])
-        # TODO: should i return (and therefore predict) noise or scaled_noise?
         return rescaled_x + scaled_noise, noise
 
     def denoise_one_step(self, x, t):
-        # from original ddpm paper
+        # from original ddpm paper (Algorithm 2)
         noise_pred = self.model(x, t)
         c1 = 1 / torch.sqrt(self.alpha_hats[t])
         c2 = (1 - self.alpha_hats[t]) / torch.sqrt(1 - self.alpha_hats[t])
         beta = self.betas[t] if t > 1 else 0
-        return c1 * (x - c2 * noise_pred) + torch.sqrt(beta) * torch.randn_like(x)
+        return c1 * (x - c2 * noise_pred) + torch.sqrt(beta) * torch.randn_like(x).to(
+            self.device
+        )
 
     def denoise_fully(self, x, steps=1000):
-        steps = max(steps, self.t_steps)
-        for i in reversed(range(1, steps)):
-            x = self.denoise_one_step(x, i)
+        # from original ddpm paper (Algorithm 2)
+        self.model.eval()
+        with torch.no_grad():
+            steps = max(steps, self.t_steps)
+            for i in reversed(range(1, steps)):
+                x = self.denoise_one_step(x, i)
+        self.model.train()
         return x
 
     def train_diffusion_model(self, images):
@@ -69,6 +75,7 @@ class DiffusionModel:
             for i, x in enumerate(train_images):
                 x.to(self.device)
                 optimizer.zero_grad()
+                # next 3 lines equate to original ddpm paper Algorithm 1 steps 3-5
                 t = self.sample_time()
                 x_noisy, noise = self.add_noise_one_step(x, t)
                 noise_pred = self.model(x_noisy, t)
@@ -100,9 +107,7 @@ class DiffusionModel:
                 ),
                 torchvision.transforms.RandomHorizontalFlip(),
                 torchvision.transforms.ToTensor(),
-                torchvision.transforms.Normalize(
-                    (0.485, 0.456, 0.406), (0.23, 0.23, 0.23)
-                ),
+                torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
             ]
         )
 
